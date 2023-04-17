0353e3bfcb89659bb4a0e58909b7ad0f4bd442d7
A8250092 <stanley.mei08@gmail.com>
2023-04-17 19:09:28 +0800

    Update practice3.py

--- Linear-Algebra/practice3.py
+++ Linear-Algebra/practice3.py
@@ -1,5 +1,19 @@
 import numpy as np
 
+def MultiplyRow(M,row_num,row_num_multiple):
+    M_new=M.copy()
+    M_new[row_num]=M_new[row_num]*row_num_multiple
+    return M_new
+
+def AddRow(M,row_num_1,row_num_2,row_num_1_multiple):
+    M_new=M.copy()
+    M_new[row_num_2]=row_num_1_multiple*M_new[row_num_1]+M_new[row_num_2]
+    return M_new
+
+def SwapRows(M,row_num_1,row_num_2):
+    M_new=M.copy()
+    M_new[[row_num_1,row_num_2]]=M_new[[row_num_2,row_num_1]]
+    return M_new
 
 
 A=np.array([
@@ -23,3 +37,22 @@ print(f"Solution: {x}")
 
 d=np.linalg.det(A)
 print(f"Determinant of matrix A is: {d:.2f}")
+
+A_system=np.hstack((A,b.reshape(3,1)))
+print(A_system)
+
+
+print("Original matrix:")
+print(A_system)
+print("Matrix after MultiplyRow:")
+print(MultiplyRow(A_system,2,2))
+
+print("Original matrix:")
+print(A_system)
+print("Matrix after AddRow:")
+print(AddRow(A_system,1,2,1/2))
+
+print("Original matrix:")
+print(A_system)
+print("Matrix after SwapRows:")
+print(SwapRows(A_system,0,2))
