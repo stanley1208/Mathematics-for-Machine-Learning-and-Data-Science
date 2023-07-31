71b441ace4e760eeaec68d6262db1ee8e255c47b
A8250092 <stanley.mei08@gmail.com>
2023-07-31 17:41:16 +0800

    Update Linear Transformations.py

--- Linear-Algebra/week3/Linear Transformations.py
+++ Linear-Algebra/week3/Linear Transformations.py
@@ -38,4 +38,25 @@ w=L(v)
 print("Original vector:\n",v,"\n Result of the transformation:\n",w)
 
 
+def T_hscaling(v):
+    A=np.array([[2,0],[0,1]])
+    w=A@v
+    return w
+
+def transform_vectors(T,v1,v2):
+    V=np.hstack((v1,v2))
+    W=T(V)
+    return W
+
+e1=np.array([[1],[0]])
+e2=np.array([[0],[1]])
+
+
+transformation_result_hscaling=transform_vectors(T_hscaling,e1,e2)
+print(e1)
+print(e2)
+print(transformation_result_hscaling)
+
+
+
 
