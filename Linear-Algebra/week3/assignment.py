7d068a744c70bbed548a44ddc26266fba22083f9
A8250092 <stanley.mei08@gmail.com>
2023-08-14 17:43:15 +0800

    Update assignment.py

--- Linear-Algebra/week3/assignment.py
+++ Linear-Algebra/week3/assignment.py
@@ -57,3 +57,37 @@ def layer_sizes(X, Y):
     n_y = Y.shape[0]
     ### END CODE HERE ###
     return (n_x, n_y)
+
+
+(n_x, n_y) = layer_sizes(X, Y)
+print("The size of the input layer is: n_x = " + str(n_x))
+print("The size of the output layer is: n_y = " + str(n_y))
+
+
+# GRADED FUNCTION: initialize_parameters
+
+def initialize_parameters(n_x, n_y):
+    """
+    Returns:
+    params -- python dictionary containing your parameters:
+                    W -- weight matrix of shape (n_y, n_x)
+                    b -- bias value set as a vector of shape (n_y, 1)
+    """
+
+    ### START CODE HERE ### (~ 2 lines of code)
+    W = np.random.randn(n_y,n_x)*0.01
+    b = np.zeros((n_y,1))
+    ### END CODE HERE ###
+
+    assert (W.shape == (n_y, n_x))
+    assert (b.shape == (n_y, 1))
+
+    parameters = {"W": W,
+                  "b": b}
+
+    return parameters
+
+
+parameters = initialize_parameters(n_x, n_y)
+print("W = " + str(parameters["W"]))
+print("b = " + str(parameters["b"]))
