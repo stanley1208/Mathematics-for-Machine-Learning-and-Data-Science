949e5031c5110b13d366621d116a1676e165d27c
stanley1208 <stanley.mei08@gmail.com>
2024-06-06 16:46:01 +0800

    Update Eigenvalues and Eigenvectors.py

--- Linear-Algebra/week4/Eigenvalues and Eigenvectors.py
+++ Linear-Algebra/week4/Eigenvalues and Eigenvectors.py
@@ -18,3 +18,14 @@ print("\n")
 
 print(f"Matrix A:\n{A} \n\nEigenvalues of matrix A:\n{A_eig[0]}\n\nEigenvectors of matrix A:\n{A_eig[1]}")
 utils.plot_transformation(A, A_eig[1][:,0], A_eig[1][:,1]);
+
+
+# define transformation matrix as a numpy array
+A_reflection_yaxis=np.array([[-1,0],[0,1]])
+# find the eigenvalues and eigenvectors of matrix
+A_reflection_yaxis_eig=np.linalg.eig(A_reflection_yaxis)
+
+print(f'Matrix A_reflection_yaxis:\n{A_reflection_yaxis}')
+print(f'Eigenvalues {A_reflection_yaxis_eig[0]}')
+print(f'Eigenvectors {A_reflection_yaxis_eig[1]}')
+utils.plot_transformation(A_reflection_yaxis, A_reflection_yaxis_eig[1][:,0], A_reflection_yaxis_eig[1][:,1]);
