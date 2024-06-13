85b4332c89d6c60f6924f32cb910243d1b57c61e
stanley1208 <stanley.mei08@gmail.com>
2024-06-13 19:09:28 +0800

    Update Eigenvalues and Eigenvectors.py

--- Linear-Algebra/week4/Eigenvalues and Eigenvectors.py
+++ Linear-Algebra/week4/Eigenvalues and Eigenvectors.py
@@ -29,3 +29,20 @@ print(f'Matrix A_reflection_yaxis:\n{A_reflection_yaxis}')
 print(f'Eigenvalues {A_reflection_yaxis_eig[0]}')
 print(f'Eigenvectors {A_reflection_yaxis_eig[1]}')
 utils.plot_transformation(A_reflection_yaxis, A_reflection_yaxis_eig[1][:,0], A_reflection_yaxis_eig[1][:,1]);
+
+
+# Shear in x-direction
+# define transformation matrix A_shear_x
+A_shear_x=np.array([[1,0.5],[0,1]])
+# find the eigenvalues and eigenvectors of matrix
+A_shear_x_eig=np.linalg.eig(A_shear_x)
+print(f'Matrix A_shear_x:{A_shear_x}')
+print(f'Eigenvalues of A_shear_x:{A_shear_x_eig[0]}')
+print(f'Eigenvectors of A_shear_x:{A_shear_x_eig[1]}')
+utils.plot_transformation(A_shear_x, A_shear_x_eig[1][:,0], A_shear_x_eig[1][:,1]);
+
+A_rotation=np.array([[0,1],[-1,0]])
+A_rotation_eig=np.linalg.eig(A_rotation)
+print(f'Matrix A_shear_x:{A_rotation}')
+print(f'Eigenvalues of A_shear_x:{A_rotation_eig[0]}')
+print(f'Eigenvectors of A_shear_x:{A_rotation_eig[1]}')
